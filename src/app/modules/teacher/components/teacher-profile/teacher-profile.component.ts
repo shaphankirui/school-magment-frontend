f74da5bd1e7d90826453f5313e018b38f1413e59
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-04-23 17:25:47 +0300

    update the teacher profile content

--- src/app/modules/teacher/components/teacher-profile/teacher-profile.component.ts
+++ src/app/modules/teacher/components/teacher-profile/teacher-profile.component.ts
@@ -4,9 +4,12 @@ import { TeacherService } from '../../../../shared/services/teacher.service';
 import { Router } from '@angular/router';
 import { CourseService } from '../../../../shared/services/course.service';
 import { Course } from '../../../../shared/interfaces/courses.interface';
-
-// Import jwt-decode using require
-const jwtDecode = {};
+import { TeacherCourseService } from '../../../../shared/services/teacher-course.service';
+import { ExamsService } from '../../../../shared/services/exams.service';
+import { Exams } from '../../../../shared/interfaces/exams.interface';
+import { ClassService } from '../../../../shared/services/class.service';
+import { Class } from '../../../../shared/interfaces/class.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-teacher-profile',
@@ -24,11 +27,17 @@ export class TeacherProfileComponent implements OnInit {
     passowrd: '',
   };
   courses: Course[] = [];
+  classes: Class[] = [];
+  teacherCourses: any[] = [];
+  teacherExams: Exams[] = [];
+  classTeacherClass: any;
 
   constructor(
     private teacherService: TeacherService,
     private coursesService: CourseService,
-    private router: Router
+    private classService: ClassService,
+    private teacherCoursesService: TeacherCourseService,
+    private examsService: ExamsService
   ) {}
 
   ngOnInit(): void {
@@ -41,21 +50,69 @@ export class TeacherProfileComponent implements OnInit {
         .subscribe((teacher: Teacher) => {
           this.teacher = teacher;
           console.log('loged in teacher ', this.teacher);
+          this.getTeacherCourses();
+          this.getTeacherExams(teacher.id);
+          this.getAllClasses();
         });
     }
+    this.getAllCourses();
   }
-
   getTeacherById(teacherId: number): void {
     this.teacherService.getTeacherById(teacherId).subscribe((teacher) => {
       this.teacher = teacher;
     });
   }
-  // getTeacherCourses() {
-  //   this.coursesService.getAllCourse().subscribe((courses) => {
-  //     this.courses = courses.filter(
-  //       (course) => course.teacherId === this.teacher.id
-  //     );
-  //     console.log('courses ', this.courses);
-  //   });
-  // }
+
+  getTeacherCourses() {
+    this.teacherCoursesService.getAllTeacherCourses().subscribe((courses) => {
+      this.teacherCourses = courses.filter(
+        (course) => course.teacherId === this.teacher.id
+      );
+    });
+  }
+  getAllCourses() {
+    this.coursesService.getAllCourse().subscribe((courses) => {
+      this.courses = courses;
+    });
+  }
+  getCourseNameById(id: number): string {
+    const courseName = this.courses.find((course) => course.id === id);
+    if (courseName) {
+      return courseName.name;
+    } else {
+      return 'loading...';
+    }
+  }
+  getTeacherExams(id: number) {
+    this.examsService.getAllExams().subscribe((exams) => {
+      this.teacherExams = exams.filter((exam) => exam.teacherId === id);
+    });
+  }
+  getAllClasses() {
+    this.classService.getAllClasses().subscribe(
+      (data) => {
+        this.classes = data;
+        console.log('All classes', this.classes);
+        this.getClassTeacherClass(
+          this.teacher.firstName,
+          this.teacher.lastName
+        );
+      },
+      (error) => {
+        console.error('Error fetching classes:', error);
+      }
+    );
+  }
+  getClassTeacherClass(firstName: string, lastName: string) {
+    const teacherFullName = `${firstName} ${lastName}`;
+    console.log('Teacher full name:', teacherFullName);
+    console.log('Checking if classes are loaded:', this.classes);
+    this.classTeacherClass =
+      this.classes.find(
+        (classItem) =>
+          classItem.classTeacher.toLowerCase().trim() ===
+          teacherFullName.toLowerCase().trim()
+      ) || [];
+    console.log("Class teacher's class:", this.classTeacherClass);
+  }
 }
